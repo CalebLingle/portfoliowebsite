5aa355ef0de81f265b02fc8deb9030bf87ceccc7
Caleb Lingle <rocky2187@gmail.com>
2021-02-05 17:39:40 -0500

    Fixing form submission handling

--- JS/handleFormSubmit.js
+++ JS/handleFormSubmit.js
@@ -1,12 +1,14 @@
-document.querySelector("form").addEventListener("submit", handleSubmit);
+$(document).ready(function() {
+    document.querySelector("form").addEventListener("submit", handleSubmit);
 
-const handleSubmit = (e) => {
-    e.preventDefault()
-    let contactForm  = document.getElementById('Contact-Form');
-    let formData = new FormData(contactForm)
-    fetch('/', {
-        method: 'POST',
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData).toString()
-    }).then(() => console.log('Form successfully submitted')).catch((error) => alert(error))
-}
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let contactForm  = document.getElementById('Contact-Form');
+        let formData = new FormData(contactForm)
+        fetch('/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString()
+        }).then(() => console.log('Form successfully submitted')).catch((error) => alert(error))
+    }
+})
